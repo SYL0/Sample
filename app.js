9adaddbf4999538b3cbc8f950365d89427fb9c17
Saber <svb3r@protonmail.com>
2019-10-08 13:13:36 +0100

    testing

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
 console.log("Hello");
 
+for (i = 0; i < 100; i++) {
+  console.log("Loading");
+}
+
+const anime = {
+  name: "Deathnote",
+  episodes: 38,
+  genre: "Thriller"
+};
+console.log(anime.name);
+
+const bestAnimes = ["One Punch Man", "Full metal Alchemist: Brotherhood"];
+console.log(bestAnimes);
+console.log(bestAnimes[0]);
+
+const bestAnime = bestAnimes[0];
+console.log(bestAnime);
